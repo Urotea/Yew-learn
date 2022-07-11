e21747eb0bb3a81128e390c8b76bfe6e72258c7d
Kakimori Takao <okakikakikaki@gmail.com>
2022-07-11 17:14:52 +0900

    とりあえず動くようになった

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello World" }</h1>
+    }
+}
 
 fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    yew::start_app::<App>();
 }
